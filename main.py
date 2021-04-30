717e652e41638a23684a327c2297810b7533d9ce
Shawn Lin <xiaoliang.lin@columbia.edu>
2021-04-30 00:37:19 -0400

    add change to utc code

--- main.py
+++ main.py
@@ -27,8 +27,7 @@ while True:
         try:
             data = auth_client.get_product_historic_rates(
                 ticker, granularity=60)
-            now = datetime.now()
-            print(f"{datetime.strftime(now, '%H:%M:%S')}: {ticker} downloaded.")
+            now = datetime.utcnow()
             if not data:
                 continue
 
@@ -37,11 +36,13 @@ while True:
             df["datetime"] = pd.to_datetime(df["datetime"], unit='s')
             df = df.sort_values(by=["datetime"])
             df["modified"] = now
+            df["modified"] = df["modified"].dt.tz_localize("UTC")
 
             simple_path = f"data/data_simple/{ticker}.csv"
             if isfile(simple_path):
                 df_old = pd.read_csv(simple_path)
                 df_old["datetime"] = pd.to_datetime(df_old["datetime"])
+                df_old["modified"] = pd.to_datetime(df_old["modified"])
                 df_old = df_old[df_old["datetime"] < df["datetime"][0]]
                 df_write = df_old.append(df)
                 df_write.to_csv(simple_path, index=False)
@@ -64,6 +65,8 @@ while True:
             else:
                 df.to_csv(full_path, index=False)
 
+            print(f"{datetime.strftime(now, '%H:%M:%S')}: {ticker} downloaded.")
+
         except Exception:
             print(f"{datetime.strftime(now, '%H:%M:%S')}: {ticker} failed!")
             pass
@@ -71,3 +74,25 @@ while True:
         sleep(0.5)
 
     sleep(30)
+
+
+# Fix US datetime to UTC
+for ticker in download_list:
+    simple_path = f"data/data_simple/{ticker}.csv"
+    if isfile(simple_path):
+        df_old = pd.read_csv(simple_path)
+        df_old["datetime"] = pd.to_datetime(df_old["datetime"])
+        df_old["modified"] = pd.to_datetime(df_old["modified"])
+        df_old["modified"] = df_old["modified"].dt.tz_localize(
+            'US/Eastern').dt.tz_convert("UTC")
+        df_old.to_csv(simple_path, index=False)
+
+    full_path = f"data/data_full/{ticker}.csv"
+
+    if isfile(full_path):
+        df_old = pd.read_csv(full_path)
+        df_old["datetime"] = pd.to_datetime(df_old["datetime"])
+        df_old["modified"] = pd.to_datetime(df_old["modified"])
+        df_old["modified"] = df_old["modified"].dt.tz_localize(
+            'US/Eastern').dt.tz_convert("UTC")
+        df_old.to_csv(full_path, index=False)
